d0e1b9e5b2d1c59d0552d436adffedc26a5e5138
yan.qi02 <yan.qi02@ele.me>
2016-04-14 14:23:24 +0800

    ADD: redux-thunk middleware

--- app/main.js
+++ app/main.js
@@ -4,20 +4,16 @@ import DockMonitor from 'redux-devtools-dock-monitor'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { routerReducer, routerMiddleware, syncHistoryWithStore } from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import * as reducers from './pages/reducers'
 import index from './pages/containers/index'
 import home from './pages/containers/home'
 import { Foo, Bar } from './pages/components'
-
-const reducer = combineReducers({
-  ...reducers,
-  routing: routerReducer
-})
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
@@ -25,10 +21,22 @@ const DevTools = createDevTools(
   </DockMonitor>
 )
 
+const reducer = combineReducers({
+  ...reducers,
+  routing: routerReducer
+})
+
 const store = createStore(
   reducer,
-  DevTools.instrument()
+  DevTools.instrument(),
+  compose(
+    applyMiddleware(
+      thunkMiddleware,
+      routerMiddleware(browserHistory)
+    )
+  )
 )
+
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
